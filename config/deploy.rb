b54d34cd9f88485fce0418e02635aba5696d1bb8
Ryan Karl <rawrnix@rawrnix.com>
2011-07-22 19:42:33 -0400

    changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,3 @@
-require "bundler/capistrano"
 set :application, "junethack"
 set :repository,  "git://github.com/junethack/Junethack"
 set :scm, :git
@@ -7,26 +6,15 @@ set :deploy_to, "~/Junethack"
 set :user, "junethack"
 set :branch, "cap-test"
 ssh_options[:keys] = [File.join(ENV["HOME"], "junethack.pem")]
-role :web, "50.17.55.154"                          # Your HTTP server, Apache/etc
-role :app, "50.17.55.154"                          # This may be the same as your `Web` server
-role :db,  "50.17.55.154", :primary => true # This is where Rails migrations will run
+role :web, "50.19.38.19"                          # Your HTTP server, Apache/etc
+role :app, "50.19.38.19"                          # This may be the same as your `Web` server
+role :db,  "50.19.38.19", :primary => true # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-namespace :setup do
-    task :essentials do
-	run "#{try_sudo} yum -y install rubygems"
-    end
-    task :bundler do
-	run "#{try_sudo} gem install bundler"
-    end
-    task :gems do
-	run "#{try_sudo} bundle install"
-    end
-end
 
 # namespace :deploy do
 #   task :start {}
